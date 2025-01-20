64fdc2455506866a747dc1487cf4aa9e9116f5c9
Asad Ali <asadali@Asads-MBP.powerhub>
2025-01-20 11:14:38 +0000

    basic netflix project setup

--- 05_api_interaction/03_login_logout/src/configs/constants.js
+++
@@ -1,3 +0,0 @@
-export const restImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtdQWcdUQREjgvXjsAw6n1yGZEKkc9NsocNQ&s";
-export const cartImage= "https://www.freeiconspng.com/thumbs/cart-icon/basket-cart-icon-27.png";
-export const logoUrl = "https://static.vecteezy.com/system/resources/previews/011/468/885/non_2x/food-logo-spoon-fork-icon-illustration-symbol-for-fast-delivery-app-restaurant-template-free-vector.jpg";
